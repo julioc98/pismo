cb822203fb62e161f94c76921439d54fe5aad4e3
“julioc98” <julio.santos@mercadolivre.com>
2020-06-27 20:06:12 -0300

    add transactions

--- cmd/api/router/router.go
+++ cmd/api/router/router.go
@@ -10,3 +10,9 @@ func SetAccountRoutes(ah handler.AccountHandler, r *mux.Router) {
 	r.HandleFunc("", ah.Add).Methods("POST")
 	r.HandleFunc("/{id:[0-9]+}", ah.FindByID).Methods("GET")
 }
+
+// SetTransactionRoutes add routes from Transaction
+func SetTransactionRoutes(th handler.TransactionHandler, r *mux.Router) {
+	r.HandleFunc("", th.Add).Methods("POST")
+	r.HandleFunc("/{id:[0-9]+}", th.FindByID).Methods("GET")
+}
---
+++ internal/app/transaction/repository.go
@@ -0,0 +1,33 @@
+package transaction
+
+import "github.com/jinzhu/gorm"
+
+type postgresRepository struct {
+	db *gorm.DB
+}
+
+//NewPostgresRepository create new postgres repository
+func NewPostgresRepository(db *gorm.DB) Repository {
+	return &postgresRepository{
+		db,
+	}
+}
+
+// Create Transaction
+func (r *postgresRepository) Create(a *Transaction) (int, error) {
+	if dbc := r.db.Create(a); dbc.Error != nil {
+		return 0, dbc.Error
+	}
+	return a.ID, nil
+}
+
+// Get Transaction
+func (r *postgresRepository) Get(id int) (*Transaction, error) {
+	var transaction Transaction
+
+	if dbc := r.db.Set("gorm:auto_preload", true).First(&transaction, id); dbc.Error != nil {
+		// if dbc := r.db.Preload("Accounts").Preload("Operations").First(&transaction, id); dbc.Error != nil {
+		return nil, dbc.Error
+	}
+	return &transaction, nil
+}
